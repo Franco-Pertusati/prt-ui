31171e0cde0441f26edc5e9099174482d1d392e5
Franco Pertusati <pertusatifranco836@gmail.com>
2025-12-31 10:26:15 -0300

    pagina de card

--- src/app/modules/docs/routes/card/card.component.ts
+++ src/app/modules/docs/routes/card/card.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CardHeaderComponent } from "../../../../prt-ui/prt-card/card-header/card-header.component";
+import { CardBodyComponent } from "../../../../prt-ui/prt-card/card-body/card-body.component";
+import { CardFooterComponent } from "../../../../prt-ui/prt-card/card-footer/card-footer.component";
+import { CardContainerComponent } from "../../../../prt-ui/prt-card/card-container/card-container.component";
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CardHeaderComponent, CardBodyComponent, CardFooterComponent, CardContainerComponent],
   templateUrl: './card.component.html'
 })
 export class CardComponent {
