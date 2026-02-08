e9f3be97f5fc3599ac3198ffebc9accf9c386b63
Franco Pertusati <pertusatifranco836@gmail.com>
2026-02-08 17:13:21 -0300

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { docsRoutes } from './modules/docs/docs.routes';
 import { SalonComponent } from './modules/salon/salon.component';
 import { salonRoutes } from './modules/salon/salon.routes';
 import { HomeComponent } from './modules/home/home.component';
+import { MoneyAppComponent } from './modules/money-app/money-app.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +20,10 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'money',
+    component: MoneyAppComponent,
   },
   {
     path: '',
---
+++ src/app/modules/money-app/money-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-money-app',
+  imports: [],
+  templateUrl: './money-app.component.html'
+})
+export class MoneyAppComponent {
+
+}
