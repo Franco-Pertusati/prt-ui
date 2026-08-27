05d774deb84ed7f39fcfb7121659c120ea8d04f3
Franco Pertusati <pertusatifranco836@gmail.com>
2026-08-27 17:02:54 -0300

    avance en portfolio de jose

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SalonComponent } from './modules/salon/salon.component';
 import { salonRoutes } from './modules/salon/salon.routes';
 import { HomeComponent } from './modules/home/home.component';
 import { TipAuditComponent } from './modules/tip-audit/tip-audit.component';
+import { JosePortfolioComponent } from './modules/jose-portfolio/jose-portfolio.component';
 
 export const routes: Routes = [
   {
@@ -24,6 +25,10 @@ export const routes: Routes = [
   {
     path: 'audit',
     component: TipAuditComponent,
+  },
+  {
+    path: 'jose',
+    component: JosePortfolioComponent,
   },
   {
     path: '',
--- src/app/modules/jose-portfolio/jose-portfolio.component.ts
+++ src/app/modules/jose-portfolio/jose-portfolio.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { JspHeaderComponent } from "./components/jsp-header/jsp-header.component";
+import { ThemeToggleBtnComponent } from "../../prt-ui/theme-toggle-btn/theme-toggle-btn.component";
+import { JspSkillsComponent } from "./components/jsp-skills/jsp-skills.component";
+import { JspArticleListComponent } from "./components/jsp-article-list/jsp-article-list.component";
 
 @Component({
   selector: 'app-jose-portfolio',
-  imports: [],
+  imports: [JspHeaderComponent, ThemeToggleBtnComponent, JspSkillsComponent, JspArticleListComponent],
   templateUrl: './jose-portfolio.component.html'
 })
 export class JosePortfolioComponent {
